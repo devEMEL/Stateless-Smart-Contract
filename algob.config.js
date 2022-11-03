a11cf477b7113c219fae6f209524aaf2fdc2f133
devEMEL <dominichenry882@gmail.com>
2022-11-03 17:27:39 +0000

    changed accounts

--- algob.config.js
+++ algob.config.js
@@ -3,13 +3,21 @@
 const { mkAccounts } = require("@algo-builder/algob");
 let accounts = mkAccounts([
   {
-    // This account is created using `make setup-master-account` command from our
-    // `/infrastructure` directory. It already has many ALGOs
+    
     name: "master",
-    addr: "N4Z7NBMODQGA6M62MBBHSJT2ZX4MBFR45ZZ45LAEBYXGJ2UTOOSPREXU4E",
-    mnemonic:
-    "erode typical couple maid must luxury tragic grape flee issue limit damage emotion dice soup quiz clay gate paddle return rifle chimney account ability invest"
+    addr: process.env.ADDR_CREATOR,
+    mnemonic: process.env.MNEMONIC_CREATOR,
   },
+  {
+    name: "acc1",
+    addr: process.env.ACC1_ADDR,
+    mnemonic: process.env.ACC1_MNEMONIC,
+  },
+  {
+    name: "acc2",
+    addr: process.env.ACC2_ADDR,
+    mnemonic: process.env.ACC2_MNEMONIC,
+  }
 ]);
 
 
